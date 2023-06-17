f4f4758c2697c83f64aac0aefb94ca8f0049a6ec
Steven Clontz <steven.clontz@gmail.com>
2023-06-17 16:40:15 +0000

    add loading skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,21 @@
 <script>
-    import 'papercss/dist/paper.min.css'
+    import 'papercss/dist/paper.min.css';
+    import { onMount } from 'svelte';
+    import { Skeleton } from 'spaper';
+    onMount(() => {
+        setInterval(() => loading = false, 500);
+    })
+    let loading = true;
 </script>
   
 <main class="container container-lg paper">
     <h1>Let's Split Up, Gang!</h1>
-    <slot/>
+    {#if loading}
+        <Skeleton active={loading} --width="5rem" --height="3rem" />
+        <Skeleton active={loading} count={6} />
+        <Skeleton active={loading} count={2} inline
+                  --width="6rem" --height="2.5rem" />
+    {:else}
+        <slot/>
+    {/if}
 </main>
