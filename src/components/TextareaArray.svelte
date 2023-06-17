fcaa6696e10e84170f1d3e6bd31f4b9adf1e164f
Steven Clontz <steven.clontz@gmail.com>
2023-06-17 20:54:29 +0000

    no duplicate names

--- src/components/TextareaArray.svelte
+++ src/components/TextareaArray.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     export let value:string[];
     let rawValue:string=value.join("\n");
-    $: value = rawValue.split("\n").filter(p=>p!=="")
+    $: value = [...new Set(rawValue.split("\n").filter(p=>p!==""))]
 </script>
 <textarea 
     style={`resize:none;width:100%;height:${(rawValue.split("\n").length+2)*1.15}em`} 
